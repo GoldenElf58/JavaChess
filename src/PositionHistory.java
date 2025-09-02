8c36e9c8c90fd81116bc069f12adf998a0b57997
Owen-FTC <27orinne@gmail.com>
2025-09-02 19:29:36 -0400

    GameState Optimizations Now uses a linked list for move history instead of a hash map

---
+++ src/PositionHistory.java
@@ -0,0 +1,26 @@
+public class PositionHistory {
+    public int positionHash;
+    public int count;
+    public PositionHistory parent;
+
+    public PositionHistory(int positionHash, PositionHistory parent, int count) {
+        this.positionHash = positionHash;
+        this.count = count;
+        this.parent = parent;
+    }
+
+    public PositionHistory(int positionHash, PositionHistory parent) {
+        this.positionHash = positionHash;
+        this.parent = parent;
+        this.count = 1;
+        PositionHistory tmp = this.parent;
+        while (tmp.parent != null && this.count < 3) {
+            if (tmp.positionHash == positionHash) this.count++;
+            tmp = tmp.parent;
+        }
+    }
+
+    public PositionHistory(int positionHash) {
+        this(positionHash, null, 1);
+    }
+}
